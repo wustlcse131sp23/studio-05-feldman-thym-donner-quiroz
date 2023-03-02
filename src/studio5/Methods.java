4915677e0f3a5595f4673bc7467dc9a2633726e1
benjaminthym <benjaminthym@localhost>
2023-03-02 12:47:24 -0600

    mariana carried

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,8 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance=0;
+		distance = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -34,17 +35,22 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x,y,(3.0/4.0)*radius);
 
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x,y,(1.0/2.0)*radius);
 
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x,y,(1.0/4.0)*radius);
 		
 	}
 
@@ -63,6 +69,12 @@ public class Methods {
 		String result = "";
 		// TODO: Finish this method
 		
+		String text = "hello" ;
+		char ch = '!';
+		String chtext = text + ch;
+		System.out.println(chtext);
+		
+	
 		return result;
 	}
 
